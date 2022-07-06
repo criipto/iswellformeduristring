d8ae185af792b8bdc0d867886ace335bd7316d60
Mikkel Christensen <christensen.mikkel@gmail.com>
2022-07-06 10:54:57 +0200

    Sample projects on NET FX 4.8 and dotnet 6.0 showcasing the
    Uri.IsWellFormedUriString behavior.

---
+++ IsWellFormedUriFX/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsWellFormedUriFX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FX 4.8");
+            Console.WriteLine("Uri.IsWellFormedUriString(\"https://example.com?a=%3a&b=%c3%b8\", UriKind.Absolute) ? {0}", Uri.IsWellFormedUriString("https://example.com?a=%3a&b=%c3%b8", UriKind.Absolute));
+            Console.WriteLine("Uri.IsWellFormedUriString(\"https://example.com?a=%3a\", UriKind.Absolute) ? {0}", Uri.IsWellFormedUriString("https://example.com?a=%3a", UriKind.Absolute));
+            Console.WriteLine("Uri.IsWellFormedUriString(\"https://example.com?b=%c3%b8\", UriKind.Absolute) ? {0}", Uri.IsWellFormedUriString("https://example.com?b=%c3%b8", UriKind.Absolute));
+        }
+    }
+}
---
+++ IsWellformedUriCore/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Core 6.0");
+Console.WriteLine("Uri.IsWellFormedUriString(\"https://example.com?a=%3a&b=%c3%b8\", UriKind.Absolute) ? {0}", Uri.IsWellFormedUriString("https://example.com?a=%3a&b=%c3%b8", UriKind.Absolute));
+Console.WriteLine("Uri.IsWellFormedUriString(\"https://example.com?a=%3a\", UriKind.Absolute) ? {0}", Uri.IsWellFormedUriString("https://example.com?a=%3a", UriKind.Absolute));
+Console.WriteLine("Uri.IsWellFormedUriString(\"https://example.com?b=%c3%b8\", UriKind.Absolute) ? {0}", Uri.IsWellFormedUriString("https://example.com?b=%c3%b8", UriKind.Absolute));
